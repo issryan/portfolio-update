945e157632a617ce824df7d05f8d5506b678f01f
Ryan <90571282+issryan@users.noreply.github.com>
2024-09-16 17:06:04 -0400

    added skills section and updated small portion of styling

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
-// src/pages/About.jsx
 import React from 'react';
-import './About.css'; // Import the updated CSS file
-import useScrollAnimation from '../hooks/useScrollAnimation'; // Import the custom hook
+import './About.css'; 
+import useScrollAnimation from '../hooks/useScrollAnimation'; 
+import checkmark from '../assets/checkmark.png'; 
 
 const About = () => {
   const [aboutRef, isAboutVisible] = useScrollAnimation();
@@ -14,10 +14,10 @@ const About = () => {
       <aside className="sidebar">
         <div className="sidebar-info">
           <figure className="avatar-box">
-            {/* Add your avatar image here */}
+            <img src="path/to/avatar.png" alt="Ryan Arafeh" width="80" />
           </figure>
           <div className="info-content">
-            <h1 className="name" title="Your Name">Ryan Arafeh</h1>
+            <h1 className="name" title="Ryan Arafeh">Ryan Arafeh</h1>
             <p className="title">Software Engineer</p>
           </div>
         </div>
@@ -66,7 +66,7 @@ const About = () => {
         </header>
         <section className="about-text">
           <p>
-            I'm a Software Engineering student navigating the coding maze. Currently knee-deep in algorithms and projects, I'm on a mission to make tech as cool as it can get. When I'm not wrestling with code, you'll catch me snapping pics of my daily adventures. From the hustle of the city to the chill vibes of nature, my camera's always ready. Right now, I'm gearing up to dive into the world of either data analysis or web development. Currently on the lookout for an internship, so if you've got exciting projects and a spot on your team, I'm all ears. Let's connect and explore the exciting intersections of code and creativity!
+            I'm a Software Engineering student navigating the coding maze...
           </p>
         </section>
 
@@ -79,7 +79,7 @@ const About = () => {
               </div>
               <div className="service-content-box">
                 <h4 className="service-item-title">Photography</h4>
-                <p className="service-item-text"> small paragraph here</p>
+                <p className="service-item-text">Small paragraph here.</p>
               </div>
             </li>
             <li className="service-item">
@@ -88,7 +88,7 @@ const About = () => {
               </div>
               <div className="service-content-box">
                 <h4 className="service-item-title">Graphic Design</h4>
-                <p className="service-item-text">small paragraph here</p>
+                <p className="service-item-text">Small paragraph here.</p>
               </div>
             </li>
             <li className="service-item">
@@ -97,7 +97,7 @@ const About = () => {
               </div>
               <div className="service-content-box">
                 <h4 className="service-item-title">Biking</h4>
-                <p className="service-item-text">small paragraph here</p>
+                <p className="service-item-text">Small paragraph here.</p>
               </div>
             </li>
           </ul>
@@ -105,35 +105,95 @@ const About = () => {
 
         <section className="service">
           <h3 className="service-title">Skills</h3>
-          <ul className="service-list">
-            <li className="service-item">
-              <div className="service-icon-box">
-                <img src="path/to/icon-design.svg" alt="design icon" />
+          <div className="experience-details-container">
+            <div className="details-container">
+              <h2 className="experience-sub-title">Frontend Development</h2>
+              <div className="article-container">
+                <article>
+                  <img src={checkmark} alt="Experience icon" className="icon" />
+                  <div>
+                    <h3>HTML</h3>
+                    <p>Experienced</p>
+                  </div>
+                </article>
+                <article>
+                  <img src={checkmark} alt="Experience icon" className="icon" />
+                  <div>
+                    <h3>CSS</h3>
+                    <p>Experienced</p>
+                  </div>
+                </article>
+                <article>
+                  <img src={checkmark} alt="Experience icon" className="icon" />
+                  <div>
+                    <h3>JavaScript</h3>
+                    <p>Intermediate</p>
+                  </div>
+                </article>
+                <article>
+                  <img src={checkmark} alt="Experience icon" className="icon" />
+                  <div>
+                    <h3>React</h3>
+                    <p>Intermediate</p>
+                  </div>
+                </article>
+                <article>
+                  <img src={checkmark} alt="Experience icon" className="icon" />
+                  <div>
+                    <h3>Bootstrap</h3>
+                    <p>Basic</p>
+                  </div>
+                </article>
               </div>
-              <div className="service-content-box">
-                <h4 className="service-item-title">Photography</h4>
-                <p className="service-item-text"> small paragraph here</p>
+            </div>
+            <div className="details-container">
+              <h2 className="experience-sub-title">Backend Development</h2>
+              <div className="article-container">
+                <article>
+                  <img src={checkmark} alt="Experience icon" className="icon" />
+                  <div>
+                    <h3>Python</h3>
+                    <p>Intermediate</p>
+                  </div>
+                </article>
+                <article>
+                  <img src={checkmark} alt="Experience icon" className="icon" />
+                  <div>
+                    <h3>SQL</h3>
+                    <p>Intermediate</p>
+                  </div>
+                </article>
+                <article>
+                  <img src={checkmark} alt="Experience icon" className="icon" />
+                  <div>
+                    <h3>MongoDB</h3>
+                    <p>Intermediate</p>
+                  </div>
+                </article>
+                <article>
+                  <img src={checkmark} alt="Experience icon" className="icon" />
+                  <div>
+                    <h3>Github</h3>
+                    <p>Intermediate</p>
+                  </div>
+                </article>
+                <article>
+                  <img src={checkmark} alt="Experience icon" className="icon" />
+                  <div>
+                    <h3>Node.js</h3>
+                    <p>Intermediate</p>
+                  </div>
+                </article>
+                <article>
+                  <img src={checkmark} alt="Experience icon" className="icon" />
+                  <div>
+                    <h3>Express.js</h3>
+                    <p>Intermediate</p>
+                  </div>
+                </article>
               </div>
-            </li>
-            <li className="service-item">
-              <div className="service-icon-box">
-                <img src="path/to/icon-design.svg" alt="design icon" />
-              </div>
-              <div className="service-content-box">
-                <h4 className="service-item-title">Graphic Design</h4>
-                <p className="service-item-text">small paragraph here</p>
-              </div>
-            </li>
-            <li className="service-item">
-              <div className="service-icon-box">
-                <img src="path/to/icon-design.svg" alt="design icon" />
-              </div>
-              <div className="service-content-box">
-                <h4 className="service-item-title">Biking</h4>
-                <p className="service-item-text">small paragraph here</p>
-              </div>
-            </li>
-          </ul>
+            </div>
+          </div>
         </section>
       </article>
     </section>
