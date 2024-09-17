b0055d7991801c31bcc27bc9f40979af9426e375
Ryan <90571282+issryan@users.noreply.github.com>
2024-09-17 12:40:52 -0400

    added new files for project descriptions

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,29 @@ import Homepage from './components/Homepage';
 import About from './pages/About';
 import Gallery from './pages/Gallery';
 import Projects from './pages/Projects';
+import Shift from './pages/projects/shift';
+import Flick from './pages/projects/flick';
+import BreedFinder from './pages/projects/breedfinder';
+import Survey from './pages/projects/survey';
 import Contact from './pages/Contact';
-import './App.css'; // Global CSS
+import './App.css';
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <div className="App">
-        <main>
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/gallery" element={<Gallery />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-      </div>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/project1" element={<Shift />} />
+        <Route path="/project2" element={<Flick />} />
+        <Route path="/project3" element={<BreedFinder />} />
+        <Route path="/project4" element={<Survey />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </Router>
   );
-}
+};
 
 export default App;
